118d55becf19e355918b438b5f8fd6b2313cf463
Niyazi Niyazi <niyaziniyazi@niyazis-mbp.home>
2020-03-06 14:08:44 +0000

    Updates

---
+++ precache-manifest.c8763170d280476a67cabca04d70f779.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "36b8644af4fa22d94072821023bdb3d6",
+    "url": "/allcats/static/media/Allcats_logo.36b8644a.svg"
+  },
+  {
+    "revision": "776c20a024baf6ec635bbf1bb2105155",
+    "url": "/allcats/static/media/Allcats_home.776c20a0.svg"
+  },
+  {
+    "revision": "6377002f1f82fde4986164adfb4f4ff8",
+    "url": "/allcats/static/media/Allcats_basket.6377002f.svg"
+  },
+  {
+    "revision": "240a2ba3198bbe04f0b9d5d382b356c5",
+    "url": "/allcats/static/media/Allcats_badge.240a2ba3.svg"
+  },
+  {
+    "revision": "99fd3736afc47daf933f",
+    "url": "/allcats/static/js/runtime~main.99fd3736.js"
+  },
+  {
+    "revision": "28dc7b4bffeaeb370d78",
+    "url": "/allcats/static/js/main.28dc7b4b.chunk.js"
+  },
+  {
+    "revision": "b53140df404a9e99a248",
+    "url": "/allcats/static/js/2.b53140df.chunk.js"
+  },
+  {
+    "revision": "28dc7b4bffeaeb370d78",
+    "url": "/allcats/static/css/main.909146ea.chunk.css"
+  },
+  {
+    "revision": "b1855b71168b9c4c1cb9175cf639ef76",
+    "url": "/allcats/index.html"
+  }
+];
